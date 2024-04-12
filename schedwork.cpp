aedc8492a29124b2d7fb637f1aeed0b125dd34c4
Emily Zhu <ekzhu@usc.edu>
2024-04-12 04:50:28 +0000

    schedwork, wordle edited

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool schedule_helper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int r, int c, std::vector<int>& shift_count_vec);
+bool isValid(DailySchedule& sched, const AvailabilityMatrix& avail, const size_t maxShifts, unsigned int worker_id, int r, int c, const size_t dailyNeed, int shift_count);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +38,80 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    for (unsigned int i = 0; i < avail.size(); i++){
+        std::vector<unsigned int> new_row(dailyNeed, INVALID_ID);
+        sched.push_back(new_row);
+    }
 
+    std::vector<int> shift_count_vec(avail[0].size(), 0);
+    bool sched_bool = schedule_helper(avail, dailyNeed, maxShifts, sched, 0, 0, shift_count_vec);
 
+    return sched_bool;
 
+}
 
+bool schedule_helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int r,
+    int c,
+    std::vector<int>& shift_count_vec)
+{
+    if (r == (int)(sched.size()-1) && c == (int)(dailyNeed)){
+        return true;
+    }
+
+    if (c == (int)(dailyNeed)) {
+        r++;
+        c = 0;
+    }
+
+    if (sched[r][c] != INVALID_ID){
+        return schedule_helper(avail, dailyNeed, maxShifts, sched, r, c+1, shift_count_vec);
+    }
+
+    for (unsigned int i = 0; i < avail[0].size(); i++){
+        sched[r][c] = i;
+        shift_count_vec[i] += 1;
+        if (isValid(sched, avail, maxShifts, i, r, c, dailyNeed, shift_count_vec[i])){
+            bool status = schedule_helper(avail, dailyNeed, maxShifts, sched, r, c+1, shift_count_vec);
+            if (status){
+                return true;
+            }
+        }
+
+        sched[r][c] = INVALID_ID;
+        shift_count_vec[i] -= 1;
+    }
+
+    return false;
+}
+
+bool isValid(
+    DailySchedule& sched,
+    const AvailabilityMatrix& avail,
+    const size_t maxShifts,
+    unsigned int worker_id,
+    int r,
+    int c,
+    const size_t dailyNeed,
+    int shift_count)
+{
+    if (avail[r][worker_id] == 0){
+        return false;
+    }
+    if (shift_count > (int)(maxShifts)){
+        return false;
+    }
+
+    for(unsigned int i = 0; i < (unsigned int)(dailyNeed); i++){
+      if (sched[r][i] == worker_id && (int)(i) != c){
+        return false;
+      }
+    }
+
+    return true;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordle_helper(std::string in, const std::string floating, const std::set<std::string>& dict, std::set<std::string>& final_set, int place, int space_left_count, std::string set_letters);
+bool is_valid(std::string in, const std::string& floating, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,80 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> final_set;
+    int count = 0;
+    std::string set_letters = "";
+    for (int i=0; i < (int)(in.size()); i++){
+      if (in[i] == '-'){
+        count++;
+      }
+      else{
+        set_letters += in[i];
+      }
+    }
+    wordle_helper(in, floating, dict, final_set, 0, count, set_letters);
+    return final_set;
 
 }
 
-// Define any helper functions here
+void wordle_helper(std::string in, 
+    const std::string floating, 
+    const std::set<std::string>& dict,
+    std::set<std::string>& final_set,
+    int place,
+    int space_left_count,
+    std::string set_letters)
+{
+    if (place == (int)(in.size())){
+        if (is_valid(in, floating, dict)){
+            final_set.insert(in);
+        }
+    }
+    else{
+      if (in[place] == '-'){
+        std::string temp_floating;
+        std::string temp = in;
+        std::string letters_to_eliminate = floating + set_letters;
+
+        for (int i = 0; i < (int)(letters_to_eliminate.size()); i++){
+          size_t index = temp.find(letters_to_eliminate[i]);
+          if (index != std::string::npos){
+            temp.erase(index, 1);
+          }
+          else{
+            temp_floating += letters_to_eliminate[i];
+          }
+        }
+
+        if (space_left_count == (int)(temp_floating.size())){
+          for (int i=0; i < (int)(temp_floating.size()); i++){
+            char next = temp_floating[i];
+            in[place] = next;
+            wordle_helper(in, floating, dict, final_set, place + 1, space_left_count-1, set_letters);
+          }
+        }
+        else{
+          for (int i=0; i < 26; i++){
+              char next = 'a' + i;
+              in[place] = next;
+              wordle_helper(in, floating, dict, final_set, place + 1, space_left_count-1, set_letters);
+          }
+        }
+      }
+
+      else{
+        wordle_helper(in, floating, dict, final_set, place + 1, space_left_count, set_letters);
+      }
+    }
+}
+
+bool is_valid(std::string in,
+    const std::string& floating, 
+    const std::set<std::string>& dict)
+{
+  if (dict.find(in) == dict.end()){
+        return false;
+  }
+
+  return true;
+}
